d62f07d526069224aca662776b3f6057329b2b6b
Denis Fedorchenko <dfedorchenko@heliostech.fr>
2018-06-08 13:42:36 +0300

    Refactored

--- ex03/ex03.sh
+++ ex03/ex03.sh
@@ -2,16 +2,11 @@ if [ "$#" -ne 4 ]; then
   exit 1
 fi
 
-printf "$2 "
-grep -c $2 $1
-grep -n $2 $1 | cut -d ':' -f 1
+file=$1
+shift
 
-printf "$3 "
-grep -c $3 $1
-grep -n $3 $1 | cut -d ':' -f 1
-
-printf "$4 "
-grep -c $4 $1
-grep -n $4 $1 | cut -d ':' -f 1
-
-exit 0
+for arg in $@; do
+  printf "$arg "
+  grep -c $arg $file
+  grep -n $arg $file | cut -d ':' -f 1
+done
